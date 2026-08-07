388d9dfc8628c887aa78b92c83670c97dda34609
gvedaxi-creator <gvedaxi@gmail.com>
2026-08-07 11:42:46 +0530

    Update MyAppointmentsDesign.dart

--- MyAppointmentsDesign.dart
+++ MyAppointmentsDesign.dart
@@ -1,83 +1,189 @@
 import 'package:flutter/material.dart';
-class MyAppointmentsDesign extends StatefulWidget {
-  MyAppointmentsDesign({super.key});
-
-  @override
-  State<MyAppointmentsDesign> createState() => _MyAppointmentsDesignState();
-}
-
-class _MyAppointmentsDesignState extends State<MyAppointmentsDesign> {
-  int currentIndex=0;
-
-  void onTap(int index){
-    setState(() {
-      currentIndex = index;
-    });
-  }
+class MyAppointmentsDesign extends StatelessWidget {
+  const MyAppointmentsDesign({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text("My Appointments",style: TextStyle(color: Colors.white),)),
-        backgroundColor: Colors.green.shade900,
-        actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.arrow_back,color: Colors.white,))
-        ],
+        title: Text("My Appoinments",style: TextStyle(color:Colors.white ),),
+        backgroundColor: Color(0xFF0F5A37),
+        centerTitle: true,
+        leading: Icon(Icons.arrow_back,color: Colors.white,),
       ),
-      body: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 1),
-          itemCount: 2,
-          itemBuilder: (context,index){
-            return Container(
-              decoration: BoxDecoration(),
-            );
-          }
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Card(
+                color: Color(0xFF0F5A37),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Order ID",style: TextStyle(color: Color(0xFFE9B958)),),
+                            Text("1234567",style: TextStyle(color: Colors.white,)),
+                          ],
+                        ),
+                      ),
+                      Divider(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Appointment Date",style: TextStyle(color: Color(0xFFE9B958)),),
+                                Text("Sunday, March 7, 2023 ",style: TextStyle(color: Colors.white,)),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Appointment Time",style: TextStyle(color: Color(0xFFE9B958)),),
+                                Text("11:00 AM",style: TextStyle(color: Colors.white,)),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Divider(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Type of Service",style: TextStyle(color: Color(0xFFE9B958)),),
+                                Text("Full Body Massage",style: TextStyle(color: Colors.white,)),
+                                Text("Back Massage",style: TextStyle(color: Colors.white,)),
+                                Text("Head Massage",style: TextStyle(color: Colors.white,)),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Total Amount",style: TextStyle(color: Color(0xFFE9B958)),),
+                                Text("100.00",style: TextStyle(color: Colors.white,)),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+
+                    ],
+                  ),
+                ),
+
+
+              ),
+              Card(
+                color: Color(0xFF0F5A37),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Order ID",style: TextStyle(color: Color(0xFFE9B958)),),
+                            Text("1234567",style: TextStyle(color: Colors.white,)),
+                          ],
+                        ),
+                      ),
+                      Divider(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Appointment Date",style: TextStyle(color: Color(0xFFE9B958)),),
+                                Text("Sunday, March 7, 2023 ",style: TextStyle(color: Colors.white,)),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Appointment Time",style: TextStyle(color: Color(0xFFE9B958)),),
+                                Text("11:00 AM",style: TextStyle(color: Colors.white,)),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Divider(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Type of Service",style: TextStyle(color: Color(0xFFE9B958)),),
+                                Text("Full Body Massage",style: TextStyle(color: Colors.white,)),
+                                Text("Back Massage",style: TextStyle(color: Colors.white,)),
+                                Text("Head Massage",style: TextStyle(color: Colors.white,)),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Total Amount",style: TextStyle(color: Color(0xFFE9B958)),),
+                                Text("100.00",style: TextStyle(color: Colors.white,)),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+
+                    ],
+                  ),
+                ),
+
+
+              ),
+            ],
+          ),
+        ),
       ),
-      // Column(
-      //   children: [
-      //     Padding(
-      //       padding: const EdgeInsets.all(8.0),
-      //       child: Container(
-      //         height: 100,
-      //         width: 150,
-      //         decoration: BoxDecoration(
-      //           borderRadius: BorderRadius.circular(10),
-      //           color: Colors.green.shade900,
-      //         ),
-      //         child: Column(
-      //           children: [
-      //             Text("Order ID",style: TextStyle(color: Colors.yellowAccent),)
-      //           ],
-      //         ),
-      //       ),
-      //     )
-      //   ],
-      // ),
       bottomNavigationBar: BottomNavigationBar(
-        onTap: onTap,
-        currentIndex: currentIndex,
-        backgroundColor: Colors.white,
-          selectedItemColor: Colors.yellow,
-          unselectedItemColor: Colors.grey,
-          items:[
-            BottomNavigationBarItem(
-                label: "Home",
-                icon: Icon(Icons.home_outlined)
-            ),
-            BottomNavigationBarItem(
-                label: "Booking",
-                icon: Icon(Icons.airplane_ticket_outlined)
-            ),
-            BottomNavigationBarItem(
-                label: "Offer",
-                icon: Icon(Icons.local_offer_outlined)
-            ),
-            BottomNavigationBarItem(
-                label: "Profile",
-                icon: Icon(Icons.person_outline)
-            )
-          ],
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home_outlined),label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.confirmation_num_outlined),label: "Booking"),
+            BottomNavigationBarItem(icon: Icon(Icons.percent_rounded),label: "Offer"),
+            BottomNavigationBarItem(icon: Icon(Icons.home_outlined),label: "Home"),
+          ]
       ),
     );
   }
