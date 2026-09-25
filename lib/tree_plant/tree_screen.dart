328fc6e8b9eafbbbd9e97cad3880acde71f8613e
gvedaxi-creator <gvedaxi@gmail.com>
2026-09-25 11:20:08 +0530

    Update tree_screen.dart

--- lib/tree_plant/tree_screen.dart
+++ lib/tree_plant/tree_screen.dart
@@ -3,22 +3,41 @@ import 'package:get/get.dart';
 
 import 'package:vedaxi/tree_plant/tree_controller.dart';
 
-class TreeScreen extends StatelessWidget {
+class TreeScreen extends StatefulWidget {
   TreeScreen({super.key});
 
+  @override
+  State<TreeScreen> createState() => _TreeScreenState();
+}
+
+class _TreeScreenState extends State<TreeScreen> {
   TreeController controller = Get.put(TreeController());
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller.treeCont();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Obx(
         () => controller.isLoading.value
             ? Center(child: CircularProgressIndicator())
-            : ListView.builder(
-                itemCount: controller.treeData.length,
-                itemBuilder: (context, index) {
-                  return;
-                },
-              ),
+            : controller.treeDataList.isEmpty
+                ? Center(child: Text("No data found"))
+                : ListView.builder(
+                    itemCount: controller.treeDataList.length,
+                    itemBuilder: (context, index) {
+                      final data = controller.treeDataList[index];
+                      return ListTile(
+                        title: Text(data.name.toString()),
+                        subtitle: Text(data.description.toString()),
+                        leading: Image.network(data.image.toString()),
+                      );
+                    },
+                  ),
       ),
     );
   }
